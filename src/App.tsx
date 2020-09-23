be2a2ac475fb6dd2038ad6c24266f162e1dc14c2
douglas.dev.2020 <douglas.dev.2020@gmail.com>
2020-09-23 06:44:55 -0300

    styles2

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
- import { Provider } from 'react-redux';
- import Routes from './routes';
- 
-import store from './store';
-import GlobalStyle from './styles/global';
+import { Provider } from "react-redux";
+import Routes from "./routes";
+
+import store from "./store";
+import GlobalStyle from "./styles/global";
 
 function App() {
   return (
     <Router>
-    <Provider store={store} >
-      <Routes />
-  
-       <GlobalStyle />
-    </Provider>
+      <Provider store={store}>
+        <Routes />
+        <GlobalStyle />
+      </Provider>
     </Router>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
- 
-
 import DashBord from '../pages/DashBord';
  
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={DashBord} />
-
   </Switch>
 );
 
