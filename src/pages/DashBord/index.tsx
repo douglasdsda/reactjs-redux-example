f22b6da3dbbf89d5db54687c7cdefa9f370a34d4
douglas.dev.2020 <douglas.dev.2020@gmail.com>
2020-10-02 10:43:28 -0300

    server

--- src/pages/DashBord/index.tsx
+++ src/pages/DashBord/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 import { Container } from "./styles";
-// import Cart from "../../components/Cart";
-// import Catalog from "../../components/Catalog";
+import Cart from "../../components/Cart";
+import Catalog from "../../components/Catalog";
 const DashBord: React.FC = () => {
   return (
     <Container>
-      {/* <Catalog />
-      <Cart /> */}
+      <Catalog />
+      <Cart />
     </Container>
   );
 };
